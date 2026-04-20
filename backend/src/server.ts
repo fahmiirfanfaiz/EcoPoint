9397e7c503406b9323ab67036e334c5782e78793
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2026-04-20 21:13:34 +0700

    feat(auth): implement user registration and login functionality

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import authRoutes from "./routes/authRoute";
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
